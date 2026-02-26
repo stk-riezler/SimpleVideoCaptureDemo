06926b3f758acf46cea26b7c30c81c0567e50298
Karlheinz Stahl <stk@riezler.eu>
2026-02-26 09:11:23 +0100

    Add project files.

---
+++ Window1.xaml.cs
@@ -0,0 +1,259 @@
+namespace Simple_Video_Capture
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using VisioForge.Core.Types;
+    using VisioForge.Core.Types.Events;
+    using VisioForge.Core.Types.VideoCapture;
+    using VisioForge.Core.VideoCapture;
+
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : IDisposable
+    {
+
+
+        /// <summary>
+        /// The video capture 1.
+        /// </summary>
+        private VideoCaptureCore VideoCapture1;
+
+        /// <summary>
+        /// The disposed value.
+        /// </summary>
+        private bool disposedValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Window1"/> class.
+        /// </summary>
+        public Window1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        /// <summary>
+        /// Create engine async.
+        /// </summary>
+        private async Task CreateEngineAsync()
+        {
+            VideoCapture1 = await VideoCaptureCore.CreateAsync(VideoView1 as IVideoView);
+
+            VideoCapture1.OnError += VideoCapture1_OnError;
+            var certificatePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                "VideoCaptureSDK-Premium-Release.vflicense");
+            await VideoCapture1.SetLicenseCertificateAsync(certificatePath);
+        }
+
+        /// <summary>
+        /// Destroy engine.
+        /// </summary>
+        private void DestroyEngine()
+        {
+            if (VideoCapture1 != null)
+            {
+                VideoCapture1.OnError -= VideoCapture1_OnError;
+
+                VideoCapture1.Dispose();
+                VideoCapture1 = null;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Window loaded.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await CreateEngineAsync();
+
+            Title += $" (SDK v{VideoCapture1.SDK_Version()})";
+
+            foreach (var device in VideoCapture1.Video_CaptureDevices())
+            {
+                cbVideoInputDevice.Items.Add(device.Name);
+            }
+
+            if (cbVideoInputDevice.Items.Count > 0)
+            {
+                cbVideoInputDevice.SelectedIndex = 0;
+            }
+
+            cbVideoInputDevice_SelectionChanged(null, null);
+
+        }
+
+   
+
+        /// <summary>
+        /// Video capture 1 on error.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="ErrorsEventArgs"/> instance containing the event data.</param>
+        private void VideoCapture1_OnError(object sender, ErrorsEventArgs e)
+        {
+        }
+
+  
+
+        /// <summary>
+        /// Cb video input device selection changed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SelectionChangedEventArgs"/> instance containing the event data.</param>
+        private void cbVideoInputDevice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbVideoInputDevice.SelectedIndex != -1 && e != null && e.AddedItems.Count > 0)
+            {
+                cbVideoInputFormat.Items.Clear();
+
+                var deviceItem =
+                    VideoCapture1.Video_CaptureDevices().FirstOrDefault(device => device.Name == e.AddedItems[0].ToString());
+                if (deviceItem == null)
+                {
+                    return;
+                }
+
+                foreach (var format in deviceItem.VideoFormats)
+                {
+                    cbVideoInputFormat.Items.Add(format);
+                }
+
+                if (cbVideoInputFormat.Items.Count > 0)
+                {
+                    cbVideoInputFormat.SelectedIndex = 0;
+                    cbVideoInputFormat_SelectionChanged(null, null);
+                }
+            }
+        }
+
+  
+
+        /// <summary>
+        /// Handles the bt start click event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private async void btStart_Click(object sender, RoutedEventArgs e)
+        {
+
+            VideoCapture1.Video_Sample_Grabber_Enabled = true;
+
+            VideoCapture1.Debug_Mode = true;
+            VideoCapture1.Debug_Dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VisioForge");
+            VideoCapture1.Audio_RecordAudio = false;
+            VideoCapture1.Audio_PlayAudio = false;
+            VideoCapture1.Video_Renderer.VideoRenderer = VideoRendererMode.WPF_WinUI_Callback;
+            // apply capture params
+            VideoCapture1.Video_CaptureDevice = new VideoCaptureSource(cbVideoInputDevice.Text);
+            VideoCapture1.Video_CaptureDevice.Format = cbVideoInputFormat.Text;
+            VideoCapture1.Video_CaptureDevice.Format_UseBest = false;
+            VideoCapture1.Mode = VideoCaptureMode.VideoPreview;
+            VideoCapture1.Video_Effects_Clear();
+
+            await VideoCapture1.StartAsync();
+        }
+
+
+        /// <summary>
+        /// Handles the bt stop click event.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private async void btStop_Click(object sender, RoutedEventArgs e)
+        {
+            await VideoCapture1.StopAsync();
+        }
+
+        /// <summary>
+        /// Cb video input format selection changed.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="SelectionChangedEventArgs"/> instance containing the event data.</param>
+        private void cbVideoInputFormat_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(cbVideoInputFormat.SelectedValue?.ToString()) || string.IsNullOrEmpty(cbVideoInputDevice.SelectedValue.ToString()))
+            {
+                return;
+            }
+
+            if (cbVideoInputDevice.SelectedIndex != -1)
+            {
+                var deviceItem = VideoCapture1.Video_CaptureDevices().FirstOrDefault(device => device.Name == cbVideoInputDevice.SelectedValue.ToString());
+                if (deviceItem == null)
+                {
+                    return;
+                }
+
+                var videoFormat = deviceItem.VideoFormats.FirstOrDefault(format => format.Name == cbVideoInputFormat.SelectedValue.ToString());
+                if (videoFormat == null)
+                {
+                    return;
+                }
+            }
+        }
+
+ 
+
+        /// <summary>
+        /// Window closing.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.ComponentModel.CancelEventArgs"/> instance containing the event data.</param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DestroyEngine();
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        /// <summary>
+        /// Dispose.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    VideoCapture1?.Dispose();
+                    VideoCapture1 = null;
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Window1()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+
